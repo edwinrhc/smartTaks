213bd548716414427d170d73138eba34733f396d
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-11-02 13:43:45 -0500

    feat(JwtUtil): Implement methods -generateToken -extractUsername -validateToken

---
+++ src/main/java/com/erhc/smarttasks/util/JwtUtil.java
@@ -0,0 +1,43 @@
+package com.erhc.smarttasks.util;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "superclaveultrasegura1234567890superultrasegura";
+
+    private final Key key = Keys.hmacShaKeyFor( SECRET_KEY.getBytes());
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1h
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        }catch (JwtException e){
+            return false;
+        }
+    }
+}
