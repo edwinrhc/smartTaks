0276e818b762f9617ebd83ba48de6572e0046efd
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-11-02 13:48:21 -0500

    feat(CustomUserDetailsService): Implement methods -loadUserByUsername

---
+++ src/main/java/com/erhc/smarttasks/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.erhc.smarttasks.service.impl;
+
+import com.erhc.smarttasks.model.User;
+import com.erhc.smarttasks.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPassword(), Collections.emptyList());
+    }
+}
